f01f8577814f1e4a0360648adb80b95b6a0eec93
ivaZahor <iva.zahor@gmail.com>
2025-04-27 15:08:57 +0200

    2. ukol - nastaveni metod jednotlivych tvaru

--- src/main/java/cz/czechitas/turtle/HlavniProgram.java
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -10,7 +10,58 @@ public class HlavniProgram {
     }
 
     public void start() {
-        //TODO Tady bude kód pro kreslení želví grafiky.
+        nakresliCtverec(100, 200, 50);
+        nakresliCtverec(150, 200, 100);
+        nakresliRovnostrannyTrojuhelnik(200, 400, 130);
+        nakresliKolecko(400, 200, 60);
+        nakresliObdelnik( 100,400,30,60);
+        nakresliObdelnik(100,500,80,140);
+        nakresliPravouhlyTrojuhelnik(500,500,80,116);
     }
 
+    public void nakresliCtverec(double x, double y, int delkaStrany) {
+        zofka.setLocation(x, y);
+        for (int i = 0; i < 4; i++) {
+            zofka.move(delkaStrany);
+            zofka.turnRight(90);
+        }
+    }
+
+    public void nakresliRovnostrannyTrojuhelnik(double x, double y, int delkaStrany) {
+        zofka.setLocation(x, y);
+        for (int i = 0; i < 3; i++) {
+            zofka.move(delkaStrany);
+            zofka.turnRight(120);
+        }
+    }
+
+    public void nakresliKolecko(double x, double y, int delkaStrany) {
+        zofka.setLocation(x, y);
+        zofka.penDown();
+        for (int i = 0; i < 12; i++) {
+            zofka.move(delkaStrany);
+            zofka.turnRight(30);
+        }
+    }
+    public void nakresliObdelnik (double x, double y, int delkaKratsiStrany, int delkaDelsiStrany ) {
+        zofka.setLocation(x, y);
+            zofka.move(delkaKratsiStrany);
+            zofka.turnRight(90);
+            zofka.move(delkaDelsiStrany);
+            zofka.turnRight(90);
+            zofka.move (delkaKratsiStrany);
+            zofka.turnRight(90);
+            zofka.move(delkaDelsiStrany);
+            zofka.turnRight(90);
+    }
+    public void nakresliPravouhlyTrojuhelnik (double x, double y, int delkaKratsiStrany, int delkaDelsiStrany) {
+        zofka.setLocation(x, y);
+        zofka.turnLeft(90);
+        zofka.move(delkaKratsiStrany);
+        zofka.turnRight(135);
+        zofka.move(delkaDelsiStrany);
+        zofka.turnRight(135);
+        zofka.move(delkaKratsiStrany);
+        zofka.turnRight(180);
+    }
 }
